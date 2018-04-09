ffddb2d269fa08cf8b8849e00a20198f0ad3ba26
ljy <736187475@qq.com>
2018-04-09 09:09:51 +0800

    框架提交

---
+++ electricharge/src/main/java/com/electricharge/ElectrichargeApplication.java
@@ -0,0 +1,12 @@
+package com.electricharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectrichargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectrichargeApplication.class, args);
+	}
+}
