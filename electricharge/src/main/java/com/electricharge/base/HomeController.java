daeba26316c790e945cc44c61dd4e20cdfbd01b4
ljy <736187475@qq.com>
2018-04-10 09:56:49 +0800

    feat: 登录页面调整
    登录页面调整

--- electricharge/src/main/java/com/electricharge/base/HomeController.java
+++ electricharge/src/main/java/com/electricharge/base/HomeController.java
@@ -4,6 +4,7 @@ import org.apache.shiro.authc.ExcessiveAttemptsException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +17,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
 import java.util.Random;
 
 @Controller
@@ -30,18 +30,18 @@ public class HomeController {
 	@RequestMapping(value={"/login"},method=RequestMethod.GET)
 	public String login(Integer type, ServletRequest request){
 		if(type==null||type.intValue()==1)
-			return "login/index";//学生端
+			return "login/index";//教师端
 		else if(type.intValue()==2)
-			return "login/index2";//教师端
+			return "login/index2";//学生端
 		else if(type.intValue()==3)
 			return "login/index3";//宿管端
 
-		return "login/index";//学生端
+		return "login/index2";//学生端
 	}
 	
 	// 登录提交地址和applicationontext-shiro.xml配置的loginurl一致。 (配置文件方式的说法)
 		@RequestMapping(value="/login",method=RequestMethod.POST)
-		public String login(HttpServletRequest request, Map<String, Object> map) throws Exception {
+		public String login(HttpServletRequest request, Model map, Integer type) throws Exception {
 			System.out.println("HomeController.login()");
 			// 登录失败从request中获取shiro处理的异常信息。
 			// shiroLoginFailure:就是shiro异常类的全类名.
@@ -52,24 +52,32 @@ public class HomeController {
 			if (exception != null) {
 				if (UnknownAccountException.class.getName().equals(exception)) {
 					System.out.println("UnknownAccountException -- > 账号不存在：");
-					msg = "UnknownAccountException -- > 账号不存在：";
+					msg = "账号不存在";
 				} else if (IncorrectCredentialsException.class.getName().equals(exception)) {
 					System.out.println("IncorrectCredentialsException -- > 密码不正确：");
-					msg = "IncorrectCredentialsException -- > 密码不正确：";
+					msg = "密码不正确";
 				} else if ("kaptchaValidateFailed".equals(exception)) {
 					System.out.println("kaptchaValidateFailed -- > 验证码错误");
-					msg = "kaptchaValidateFailed -- > 验证码错误";
+					msg = "验证码错误";
 				}else if (ExcessiveAttemptsException.class.getName().equals(exception)) {
 					System.out.println("ExcessiveAttemptsException -- > 登录失败次数过多：");
-					msg = "ExcessiveAttemptsException -- > 登录失败次数过多：";
+					msg = "登录失败次数过多";
 				}   else {
 					msg = "else >> "+exception;
 					System.out.println("else -- >" + exception);
 				}
 			}
-			map.put("msg", msg);
+			map.addAttribute("msg", msg);
 			// 此方法不处理登录成功,由shiro进行处理.
-			return "login/index";
+
+			if(type==null||type.intValue()==1)
+				return "login/index";//教师端
+			else if(type.intValue()==2)
+				return "login/index2";//学生端
+			else if(type.intValue()==3)
+				return "login/index3";//宿管端
+
+			return "login/index2";//学生端
 		}
 
 		@RequestMapping(value="getValidateCode",method = RequestMethod.GET)
